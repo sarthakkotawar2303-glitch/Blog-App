50e52415fb92397a2830981e41a2fb8986b4f199
sarthak873 <sarthakkotawar@gmail.com>
2026-03-03 19:07:05 +0530

    Fix case sensitivity: renamed UserController to userController

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { SignUpUser, loginUser } = require('../controllers/userController');
-
+const {SignUpUser,loginUser} =require("../controllers/userController")
 const router = express.Router();
 
 // Auth routes
