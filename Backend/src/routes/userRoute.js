c8d8bf238ec4448023fdc895f5d5ec04582ae9b7
sarthak873 <sarthakkotawar@gmail.com>
2026-03-03 19:15:31 +0530

    fixing the issue of file name

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {SignUpUser,loginUser} =require("../controllers/userController")
+const {SignUpUser,loginUser} =require("../controllers/UserController")
 const router = express.Router();
 
 // Auth routes
